2d298e060efffe56e424d5250e67aa13b98ae9b1
faddy199494 <51315168+faddy199494@users.noreply.github.com>
2019-06-05 23:45:44 +0700

    Update bot-flex.php

--- bot-flex.php
+++ bot-flex.php
@@ -12,170 +12,123 @@ $request = file_get_contents('php://input');   // Get request content
 $request_array = json_decode($request, true);   // Decode JSON to Array
 
 $jsonFlex = [
-      "type": "bubbleFlex",
-      "name": "Bubble message 1",
-      "params": {
-        "header": {
-          "type": "box",
-          "disable": true,
-          "componentProps": [
-            "vertical",
-            0,
-            "",
-            "",
-            {
-              "type": "uri"
-            }
+      "type" => "flex",
+    "altText" => "Hello Flex Message",
+    "contents" => [
+      "type" => "bubble",
+      "direction" => "ltr",
+      "header" => [
+        "type" => "box",
+        "layout" => "vertical",
+        "contents" => [
+          [
+            "type" => "text",
+            "text" => "Purchase",
+            "size" => "lg",
+            "align" => "start",
+            "weight" => "bold",
+            "color" => "#009813"
           ],
-          "contents": []
-        },
-        "hero": {
-          "type": "image",
-          "disable": false,
-          "componentProps": [
-            "https://srw889th.com/pics/slot889th/S__8650796.jpg",
-            "none",
-            "full",
-            "none",
-            "none",
-            "1:1",
-            "cover",
-            "",
-            "",
-            {
-              "type": "uri",
-              "label": "Link register",
-              "uri": "https://line.me/R/ti/p/%40sny7726x",
-              "disable": false,
-              "mode": "datetime"
-            }
-          ]
-        },
-        "body": {
-          "type": "box",
-          "disable": false,
-          "componentProps": [
-            "vertical",
-            "none",
-            "md",
-            "",
-            {
-              "type": "uri",
-              "label": "Action",
-              "uri": "https://line.me/R/ti/p/%40sny7726x"
-            }
+          [
+            "type" => "text",
+            "text" => "฿ 100.00",
+            "size" => "3xl",
+            "weight" => "bold",
+            "color" => "#000000"
           ],
-          "contents": [
-            {
-              "type": "text",
-              "componentProps": [
-                "â™š à¸ªà¸¡à¸±à¸„à¸£à¹€à¸¥à¸¢à¸•à¸­à¸™à¸™à¸µà¹‰à¸Ÿà¸£à¸µ â™š",
-                "none",
-                "none",
-                "",
-                "bold",
-                "center",
-                "none",
-                "",
-                "none",
-                {
-                  "type": "uri"
-                }
-              ]
-            },
-            {
-              "type": "text",
-              "componentProps": [
-                "ðŸ˜ Credit 100 à¸šà¸²à¸— ðŸ˜",
-                "none",
-                "none",
-                null,
-                null,
-                "center",
-                null,
-                "",
-                "none",
-                {
-                  "type": "uri"
-                }
-              ]
-            },
-            {
-              "type": "image",
-              "disable": false,
-              "componentProps": [
-                "https://media.tenor.co/images/038350fd12d6b84d653ae64704601e2f/tenor.gif",
-                "none",
-                "none",
-                null,
-                null,
-                "none",
-                "none",
-                "",
-                null,
-                {
-                  "type": "uri"
-                }
-              ]
-            }
-          ]
-        },
-        "footer": {
-          "type": "box",
-          "disable": false,
-          "componentProps": [
-            "vertical",
-            "none",
-            "",
-            "",
-            {
-              "type": "uri"
-            }
+          [
+            "type" => "text",
+            "text" => "Rabbit Line Pay",
+            "size" => "lg",
+            "weight" => "bold",
+            "color" => "#000000"
           ],
-          "contents": [
-            {
-              "type": "spacer",
-              "componentProps": [
-                "xxl"
-              ]
-            },
-            {
-              "type": "button",
-              "componentProps": [
-                {
-                  "type": "uri",
-                  "label": "à¸ªà¸¡à¸±à¸„à¸£à¸ªà¸¡à¸²à¸Šà¸´à¸",
-                  "uri": "https://line.me/R/ti/p/%40sny7726x",
-                  "mode": "datetime"
-                },
-                "none",
-                "",
-                "none",
-                "primary",
-                "#D90D0D",
-                "none"
-              ]
-            },
-            {
-              "type": "button",
-              "componentProps": [
-                {
-                  "type": "uri",
-                  "label": "à¹€à¸‚à¹‰à¸²à¹€à¸§à¹‡à¸šà¹„à¸‹à¸•à¹Œ",
-                  "uri": "https://srw889th.com/",
-                  "mode": "datetime"
-                },
-                "none",
-                "",
-                "none",
-                "primary",
-                null,
-                null
-              ]
-            }
+          [
+            "type" => "text",
+            "text" => "2019.02.14 21:47 (GMT+0700)",
+            "size" => "xs",
+            "color" => "#B2B2B2"
+          ],
+          [
+            "type" => "text",
+            "text" => "Payment complete.",
+            "margin" => "lg",
+            "size" => "lg",
+            "color" => "#000000"
           ]
-        },
-
+        ]
+      ],
+      "body" => [
+        "type" => "box",
+        "layout" => "vertical",
+        "contents" => [
+          [
+            "type" => "separator",
+            "color" => "#C3C3C3"
+          ],
+          [
+            "type" => "box",
+            "layout" => "baseline",
+            "margin" => "lg",
+            "contents" => [
+              [
+                "type" => "text",
+                "text" => "Merchant",
+                "align" => "start",
+                "color" => "#C3C3C3"
+              ],
+              [
+                "type" => "text",
+                "text" => "BTS 01",
+                "align" => "end",
+                "color" => "#000000"
+              ]
+            ]
+          ],
+          [
+            "type" => "box",
+            "layout" => "baseline",
+            "margin" => "lg",
+            "contents" => [
+              [
+                "type" => "text",
+                "text" => "New balance",
+                "color" => "#C3C3C3"
+              ],
+              [
+                "type" => "text",
+                "text" => "฿ 45.57",
+                "align" => "end"
+              ]
+            ]
+          ],
+          [
+            "type" => "separator",
+            "margin" => "lg",
+            "color" => "#C3C3C3"
+          ]
+        ]
+      ],
+      "footer" => [
+        "type" => "box",
+        "layout" => "horizontal",
+        "contents" => [
+          [
+            "type" => "text",
+            "text" => "View Details",
+            "size" => "lg",
+            "align" => "start",
+            "color" => "#0084B6",
+            "action" => [
+              "type" => "uri",
+              "label" => "View Details",
+              "uri" => "https://google.co.th/"
+            ]
+          ]
+        ]
+      ]
+    ]
+  ];
 
 
 if ( sizeof($request_array['events']) > 0 ) {
