b5d65927e014a27d96ca32e79994b8c5ef7db8eb
faddy199494 <51315168+faddy199494@users.noreply.github.com>
2019-06-06 15:05:21 +0700

    Update bot-flex.php

--- bot-flex.php
+++ bot-flex.php
@@ -12,6 +12,7 @@ $request = file_get_contents('php://input');   // Get request content
 $request_array = json_decode($request, true);   // Decode JSON to Array
 
 $jsonFlex = [
+  {
   "type": "bubble",
   "body": {
     "type": "box",
